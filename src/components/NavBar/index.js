50247f527fe5cbfb82d552ac0bfe5485a4e27f68
Raed Salah <dkraed@gmail.com>
2019-01-11 21:29:51 +0800

    cleared rubbish and created nav bar componenet

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-light bg-light">
+          <span class="navbar-brand mb-0 h1">Navbar</span>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
